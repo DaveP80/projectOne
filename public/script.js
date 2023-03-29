b3127ae35017115c6b598359e4887b797db07a71
davep80 <davidpaq1@gmail.com>
2023-03-29 14:55:57 -0400

    test button

--- public/script.js
+++ public/script.js
@@ -1,55 +1,22 @@
-document.addEventListener('DOMContentLoaded', async function () {
-    const copyTextBtn = document.getElementById('copy-text-btn')
-    const copyImgBtn = document.getElementById('copy-img-btn')
-    const canWriteEl = document.getElementById('can-write')
-    const textarea = document.querySelector('textarea')
-    const img = document.querySelector('img')
-    const errorEl = document.getElementById('errorMsg')
-  
-    async function askWritePermission() {
-      try {
-        const { state } = await navigator.permissions.query({ name: 'clipboard-write', allowWithoutGesture: false })
-        return state === 'granted'
-      } catch (error) {
-        errorEl.textContent = `Compatibility error (ONLY CHROME > V66): ${error.message}`
-        console.log(error)
-        return false
-      }
+let grabimg = document.getElementById('centerimg')
+let copybtn = document.getElementById('copy-img-btn')
+
+copybtn.addEventListener('click', () => {
+    copyToClipboard(grabimg.src);
+});
+
+async function copyToClipboard(src) {
+    const data = await fetch(src);
+    const blob = await data.blob();
+    try {
+        await navigator.clipboard.write([
+            new ClipboardItem({
+                [blob.type]: blob,
+            })
+        ]);
+
+        console.log("Success");
+    } catch (e) {
+        console.log("Copy failed: " + e);
     }
-  
-    const canWrite = await askWritePermission()
-  
-    canWriteEl.textContent = canWrite
-    canWriteEl.style.color = canWrite ? 'green' : 'red'
-  
-    copyImgBtn.disabled = copyTextBtn.disabled = !canWrite
-  
-    const setToClipboard = blob => {
-      const data = [new ClipboardItem({ [blob.type]: blob })]
-      return navigator.clipboard.write(data)
-    }
-  
-    /**
-     * @param Blob - The ClipboardItem takes an object with the MIME type as key, and the actual blob as the value.
-     */
-  
-    copyTextBtn.addEventListener('click', async () => {
-      try {
-        const blob = new Blob([textarea.value], { type: 'text/plain' })
-        await setToClipboard(blob)
-      } catch (error) {
-        console.error('Something wrong happened')
-      }
-    })
-  
-    copyImgBtn.addEventListener('click', async () => {
-      try {
-        const response = await fetch(img.src)
-        const blob = await response.blob()
-        await setToClipboard(blob)
-      } catch (error) {
-        console.error('Something wrong happened')
-        console.error(error)
-      }
-    })
-  })
+}
