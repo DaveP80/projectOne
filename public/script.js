f1b0d72518bcf40a04af51906f41860d97b54654
davep80 <davidpaq1@gmail.com>
2023-03-29 21:08:50 -0400

    canvas and text functionality

--- public/script.js
+++ public/script.js
@@ -1,9 +1,75 @@
+async function getQuote(api_url, headers, flag) {
+  if (flag == "search") {
+      const myElement = document.querySelector('.centered');
+      const newp = document.createElement('p')
+      newp.id = 'loadingtext'
+      newp.textContent = '...loading'
+      if (myElement) myElement.appendChild(newp)
+  }
+  const response = await fetch(api_url, headers);
+
+  const data = await response.json();
+  return data;
+}
+
+const form = document.querySelector('.usersubmit');
+
+form.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  const category = form.elements.category.value;
+
+  const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '04a630e781mshefd694339487a87p171cc9jsn6bd43ac41939',
+        'X-RapidAPI-Host': 'quotes-villa.p.rapidapi.com'
+      }
+    };
+
+    getQuote(`https://quotes-villa.p.rapidapi.com/quotes/${category}`, options, "search")
+      .then(data => {
+        let deltext = document.getElementById('loadingtext')
+        deltext.remove()
+        let changearea = document.getElementById('centertext')
+        changearea.textContent = Object.values(data[3]).join('').trim().replaceAll("  ", "")
+        makeCanvas()
+      })
+      .catch(error => console.error(error));
+})
+
+
+
+
+
 let grabimg = document.getElementById('centerimg')
 let copybtn = document.getElementById('copy-img-btn')
+let textb = document.getElementById('copy-text-btn')
 
 copybtn.addEventListener('click', () => {
     copyToClipboard(grabimg.src);
 });
+
+textb.addEventListener('click', () => {
+  let divContent = document.getElementById('centertext')
+  const textToCopy = divContent.textContent
+  const textarea = document.createElement('textarea');
+  // Set the value of the textarea to the text content
+  textarea.value = textToCopy;
+
+  // Append the textarea to the body
+  document.body.appendChild(textarea);
+
+  // Select the textarea
+  textarea.select();
+
+  // Copy the text
+  document.execCommand('copy');
+
+  // Remove the textarea
+  document.body.removeChild(textarea);
+  })
 
 async function copyToClipboard(src) {
     const data = await fetch(src);
@@ -21,34 +87,34 @@ async function copyToClipboard(src) {
     }
 }
 
-// Select the canvas element
-const canvas = document.querySelector('canvas');
+function makeCanvas() {
 
-// Get the canvas context
+// Select the canvas element
+const canvas = document.getElementById('myCanvas');
+
+// Select the p element from the DOM
+const pElement = document.getElementById("centertext");
+
 const ctx = canvas.getContext('2d');
 
-// Set the background color
-ctx.fillStyle = 'gray';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Set the font properties
+const fontSize = 16;
+ctx.font = `${fontSize}px Arial`;
 
-// Set the text properties
-ctx.fillStyle = 'white';
-ctx.font = '20px sans-serif';
-ctx.textAlign = 'center';
-ctx.textBaseline = 'middle';
+// Set the canvas size to match the text size
+const textWidth = ctx.measureText(pElement.textContent).width;
+canvas.width = textWidth*0.80;
+canvas.height = fontSize * 3;
 
-// Set the text content
-const text = 'Hello, world!';
-
-// Get the text dimensions
-const textWidth = ctx.measureText(text).width;
-
-// Calculate the text position
+// Center the text
 const x = canvas.width / 2;
 const y = canvas.height / 2;
+ctx.textBaseline = 'middle';
+ctx.textAlign = 'center';
 
-// Draw the text
-ctx.fillText(text, x, y);
+// Draw the text on the canvas
+ctx.fillText(pElement.textContent, x, y);
+}
 
 let copycanvas = document.getElementById('copy-canvas-btn')
 
@@ -72,26 +138,76 @@ const newCtx = newCanvas.getContext('2d');
 newCtx.drawImage(canvas, 0, 0);
 
 // Convert the canvas content to a data URL
-const dataUrl = newCanvas.toDataURL();
+newCanvas.toBlob((blob) => {
 
-// Create a new text area element to hold the data URL
-const textArea = document.createElement('textarea');
-textArea.value = dataUrl;
+  // Create a new image element
+  const img = new Image();
 
-// Append the text area element to the DOM
-document.body.appendChild(textArea);
+  // Set the image source to the Blob URL
+  img.src = URL.createObjectURL(blob);
 
-// Select the text area
-textArea.select();
-
-// Copy the contents of the text area to the clipboard
-document.execCommand('copy');
-
-// Remove the text area from the DOM
-document.body.removeChild(textArea);
-
-
+  copyToClipboard(img.src)
+  
+});
 })
+
+function makeCanvas2(args) {
+  // Get the textarea and canvas elements
+//const textarea = document.getElementById('centertext');
+const canvas = document.getElementById('myCanvas');
+
+// Set the background color of the canvas
+canvas.style.backgroundColor = 'gray';
+
+// Get the context of the canvas
+const context = canvas.getContext('2d');
+
+// Set the font size and font family of the text
+const fontSize = 14;
+const fontFamily = 'Arial';
+context.font = `${fontSize}px ${fontFamily}`;
+
+// Measure the width and height of the text
+const lines0 = args.split('\n');
+let words = [];
+
+for (let i = 0; i < lines0.length; i++) {
+  const line = lines0[i];
+  const lineWords = line.split(' ');
+  words = [...words, ...lineWords];
+}
+const lines = [];
+let currentLine = words[0];
+for (let i = 1; i < words.length; i++) {
+  const word = words[i];
+  console.log(word);
+  const width = context.measureText(`${currentLine} ${word}`).width;
+  if (width < canvas.width) {
+    currentLine += ` ${word}`;
+  } else {
+    lines.push(currentLine);
+    currentLine = word;
+  }
+}
+lines.push(currentLine);
+const textHeight = fontSize * 1.5 * lines.length;
+
+// Set the width and height of the canvas
+const textWidth = Math.max(...lines.map(line => context.measureText(line).width));
+const canvasWidth = Math.min(textWidth, 300); // Limit the width to a maximum of 300px
+const canvasHeight = textHeight;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+// Draw the text onto the canvas
+context.fillStyle = 'black';
+let y = fontSize;
+lines.forEach(line => {
+  const x = (canvas.width - context.measureText(line).width) / 2;
+  context.fillText(line, x, y);
+  y += fontSize * 3;
+});
+}
 
 
 
